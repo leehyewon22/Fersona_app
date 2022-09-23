e2379747bc32b3fb6636360b0bef647619dcb0c8
주혜수 <106126676+juhyesu128@users.noreply.github.com>
2022-09-23 15:58:38 +0900

    Initial commit

---
+++ app/src/main/java/com/example/fersona_app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.fersona_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 깃 테스트용
+        
+    }
+}
